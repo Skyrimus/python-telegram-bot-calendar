1f038c89fbfb9793282f48bcd52098af2fb6e5a5
Skyrimus <Skyrimus@yandex.ru>
2024-08-07 20:08:28 +0300

    Update base.py

--- telegram_bot_calendar/base.py
+++ telegram_bot_calendar/base.py
@@ -157,6 +157,7 @@ class TelegramCalendar:
         if self.telethon:
             return buttons
         if self.pyrogram:
+            print("pyrogram keyboard")
             return InlineKeyboardMarkup(buttons)
         return self._build_json_keyboard(buttons)
 
